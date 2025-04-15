74b62c7f6e39679a9ca9245e27097a36d1d130ec
getsolarajs <getsolara@outlook.com>
2025-04-15 22:01:14 +0100

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -1,45 +1,50 @@
 const sidebars = {
-    docsSidebar: [
-      {
-        type: 'doc',
-        id: 'introduction',
-        label: 'Introduction',
-      },
-      {
-        type: 'category',
-        label: 'Getting Started',
-        items: [
-          'getting-started/installation',
-          'getting-started/first-bot',
-        ],
-      },
-      {
-        type: 'category',
-        label: 'Core Concepts',
-        items: [
-          'core-concepts/function-syntax',
-          'core-concepts/commands',
-          'core-concepts/interactions',
-          'core-concepts/events',
-          'core-concepts/variables',
-        ],
-      },
-      {
-        type: 'category',
-        label: 'Guides',
-        items: [
-            'guides/creating-embeds',
-            'guides/custom-functions',
-        ],
-      },
-      {
-        type: 'category',
-        label: 'Reference',
-        items: [
-          'reference/function-list',
-        ],
-      },
-    ],
-  };
+  docsSidebar: [
+    {
+      type: 'doc',
+      id: 'introduction',
+      label: 'Introduction',
+    },
+    {
+      type: 'category',
+      label: 'Getting Started',
+      items: [
+        'getting-started/installation',
+        'getting-started/first-bot',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Core Concepts',
+      items: [
+        'core-concepts/function-syntax',
+        'core-concepts/commands',
+        'core-concepts/interactions',
+        'core-concepts/events',
+        'core-concepts/variables',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Guides',
+      items: [
+          'guides/creating-embeds',
+          'guides/custom-functions',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Reference',
+      items: [
+        'reference/function-list',
+      ],
+    },
+    {
+      type: 'doc',
+      id: 'changelog',
+      label: 'Changelog',
+    },
+  ],
+};
 
-  export default sidebars;
+export default sidebars;
